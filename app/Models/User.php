112df0576fde1c02f6b77c7a7bf418833b181e45
klinson <klinson@163.com>
2018-05-28 17:32:02 +0800

    add base policy

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,15 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isSuperAdmin()
+    {
+        // 判断用户身份是否是超级管理员
+        // TODO: 待补充条件判断
+        if (0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
---
+++ app/Policies/Policy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class Policy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function before(User $user, $ability)
+    {
+        if ($user->isSuperAdmin()) {
+            return true;
+        }
+    }
+}
