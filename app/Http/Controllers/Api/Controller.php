5b7146725bf6575736a5266af690b8f8ea9a9167
klinson <klinson@163.com>
2018-05-29 11:29:59 +0800

    add dingo base controller

---
+++ app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as BaseController;
+use Dingo\Api\Routing\Helpers;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class Controller extends BaseController
+{
+    use Helpers;
+}
