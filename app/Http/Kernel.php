376c4228a8752577fc8846a3fe82f2a3b50e00bb
klinson <klinson@163.com>
2018-05-30 14:28:22 +0800

    add jwt auth refresh token middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        // 自动刷新JWT
+        'refresh.token' => \App\Http\Middleware\RefreshToken::class
     ];
 }
