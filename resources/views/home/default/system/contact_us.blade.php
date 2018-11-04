07c8457ddfd9664c43a21a35ee61311137ac546e
klinson <klinson@163.com>
2018-11-04 14:56:41 +0800

    HTML

--- resources/views/home/default/system/contact_us.blade.php
+++ resources/views/home/default/system/contact_us.blade.php
@@ -104,16 +104,15 @@
                         <div class="row form-group">
                             <div class="col-xs-6 col-sm-9 col-md-9 {{$errors->has('captcha')?'has-error':''}}">
                                 <input type="text" id="captcha" class="form-control" name="captcha" placeholder="请输入验证码" required value="{{ old('captcha') }}">
+                                @if($errors->has('captcha'))
+                                    <div class="col-md-12">
+                                        <p class="text-danger text-left"><strong>{{$errors->first('captcha')}}</strong></p>
+                                    </div>
+                                @endif
                             </div>
-                            <div class="col-xs-6 col-sm-3 col-md-3 {{$errors->has('captcha')?'has-error':''}}">
+                            <div class="col-xs-6 col-sm-3 col-md-3">
                                 <img src="{{captcha_src()}}" style="cursor: pointer" onclick="this.src='{{captcha_src()}}'+Math.random()">
                             </div>
-
-                            @if($errors->has('captcha'))
-                                <div class="col-md-12">
-                                    <p class="text-danger text-left"><strong>{{$errors->first('captcha')}}</strong></p>
-                                </div>
-                            @endif
                         </div>
                         <div class="row form-group">
                             <div class="col-md-12 text-center">
