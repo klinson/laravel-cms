2f1dcf26e76459148f826067c197dd562651e3d6
klinson <klinson@163.com>
2018-11-11 11:26:55 +0800

    详情页

--- resources/views/home/page/articles/show.blade.php
+++ resources/views/home/page/articles/show.blade.php
@@ -12,52 +12,27 @@
                 <div class="generic-banner-content">
                     <h2 class="text-white">{{ $article->title }}</h2>
                     <p class="text-white">{{ $article->description }}</p>
+                    <p class="text-white">
+                        <span>{{ $article->author }}</span>
+                        <span>发布于</span>
+                        <span>{{ $article->publish_time }}</span>
+                    </p>
                 </div>
             </div>
         </div>
     </div>
 @endsection
 
-@section('header')
-    <section id="home" class="video-hero" style="height: 500px; background-image: url({{ get_admin_file_url($article->thumbnail) }});  background-size:cover; background-position: center center;background-attachment:fixed;" data-section="home">
-        <div class="overlay"></div>
-        <div class="display-t display-t2 text-center">
-            <div class="display-tc display-tc2">
-                <div class="container">
-                    <div class="col-md-12 col-md-offset-0">
-                        <div class="animate-box">
-                            <h2>{{ $article->title }}</h2>
-                            <p class="breadcrumbs">
-                                <span><a href="{{ route('index') }}">首页</a></span>
-                                @if (! empty($category->parent))
-                                    <span><a href="{{ route('articles.categories', ['category' => $category->parent->id]) }}">{{ $category->parent->title }}</a></span>
-                                @endif
-                                <span><a href="{{ route('articles.categories', ['category' => $category->id]) }}">{{ $category->title }}</a></span>
-                                <span>{{ $article->title }}</span>
-                            </p>
-                            <h4 style="color: rgba(255, 255, 255, 0.9);">
-                                <span>{{ $article->author }}</span>
-                                <span>发布于</span>
-                                <span>{{ $article->publish_time }}</span>
-                            </h4>
-                            <h5 style="color: rgba(255, 255, 255, 0.9);">{{ $article->description }}</h5>
-                        </div>
-                    </div>
+@section('content')
+    <div class="main-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ucapcontent>
+                        {!! $article->content !!}
+                    </ucapcontent>
                 </div>
             </div>
-        </div>
-    </section>
-@endsection
-
-@section('content')
-    <div class="colorlib-about">
-        <div class="container">
-            <div class="row col-md-12">
-                <ucapcontent>
-                    {!! $article->content !!}
-                </ucapcontent>
-            </div>
-
         </div>
     </div>
 @endsection
