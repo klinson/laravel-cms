67e8ff72ff0e1e6dc6f41cd284424d9c644c7d25
klinson <klinson@163.com>
2018-11-26 18:06:46 +0800

    微信菜单种子数据

---
+++ app/Models/WechatMenu.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Encore\Admin\Traits\AdminBuilder;
+use Encore\Admin\Traits\ModelTree;
+use Illuminate\Database\Eloquent\Model;
+
+class WechatMenu extends Model
+{
+    use ModelTree, AdminBuilder;
+
+    public $timestamps = false;
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->setParentColumn('parent_id');
+        $this->setOrderColumn('sort');
+        $this->setTitleColumn('title');
+    }
+
+    public static function selectCategoryOptions()
+    {
+        return self::all(['id', 'title'])->pluck('title', 'id');
+    }
+
+    public static function getTree()
+    {
+        $list = self::orderBy('sort')->where('has_enabled', 1)->get()->toArray();
+        $tree = list_to_tree($list, 0, 'id', 'parent_id', 'children');
+        return $tree;
+    }
+}
