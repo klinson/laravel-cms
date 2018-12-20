2fe40a0b55562edbd5b540d2161390a95eb7f16b
klinson <klinson@163.com>
2018-12-20 16:24:29 +0800

    随机数方法

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -138,3 +138,59 @@ function download_wechat_temp_media($media_id)
 
     return null;
 }
+
+/**
+ * 生成随机字符串
+ * @param int $length 生成长度
+ * @param int $type 字符串类型 0-7 8种模式
+ * @author klinson <klinson@163.com>
+ * @return string
+ */
+function random_string($length = 6, $type = 0): string
+{
+    $chars = [
+        '0123456789',
+        'abcdefghijklmnopqrstuvwxyz',
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        '!@#$%^&*()-_ []{}<>~`+=,.;:/?|'
+    ];
+    $char_seeder = '';
+    switch ($type) {
+        case 1:
+            $char_seeder = $chars[1];
+            break;
+        case 2:
+            $char_seeder = $chars[2];
+            break;
+        case 3:
+            $char_seeder = $chars[3];
+            break;
+        case 4:
+            $char_seeder = $chars[0] . $chars[1];
+            break;
+        case 5:
+            $char_seeder = $chars[1] . $chars[2];
+            break;
+        case 6:
+            $char_seeder = $chars[0] . $chars[1] . $chars[2];
+            break;
+        case 7:
+            $char_seeder = $chars[0] . $chars[1] . $chars[2] . $chars[3];
+            break;
+        case 0:
+        default:
+            $char_seeder = $chars[0];
+            break;
+    }
+    $random_string = '';
+    for ( $i = 0; $i < $length; $i++ )
+    {
+        // 这里提供两种字符获取方式
+        // 第一种是使用 substr 截取$chars中的任意一位字符；
+        // 第二种是取字符数组 $chars 的任意元素
+        // $random_string .= substr($char_seeder, mt_rand(0, strlen($char_seeder) - 1), 1);
+        $random_string .= $char_seeder[ mt_rand(0, strlen($char_seeder) - 1) ];
+    }
+
+    return $random_string;
+}
