7384e398943e1369c5f02bb4bef53d7cbb67594f
klinson <klinson@163.com>
2020-04-17 02:06:56 +0800

    更新后台首页

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -17,22 +17,8 @@ class HomeController extends Controller
         return Admin::content(function (Content $content) {
             $this->_setPageDefault($content);
 
-            $content->row(Dashboard::title());
+            $content->row(view('admin.dashboard.title'));
 
-            $content->row(function (Row $row) {
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
-            });
         });
     }
 }
